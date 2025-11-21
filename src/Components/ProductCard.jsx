3df87b6783588c6540dd4aef06e2a1c31a1d06a7
Abhijith <abhijithpb2506@gmail.com>
2025-11-21 10:48:28 +0530

    Path typo fixed

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,5 +1,5 @@
 import React,{useState} from "react";
-import "../styles/App.css";
+import "../Styles/App.css";
 import { Link } from "react-router-dom";
 export function ProductCard({product,onAddToCart})
 {
